9c944162041f69da0c07a61d244713d43868d12d
Benjamin Canac <canacb1@gmail.com>
2018-03-22 20:59:36 +0100

    template: Update mongodb conf

--- template/conf/development.js
+++ template/conf/development.js
@@ -1,7 +1,8 @@
 module.exports = {
 	mono: {
 		<% if (modules.mongodb) { %>mongodb: {
-			url: 'mongodb://localhost:27017/<%= name %>'
+			url: 'mongodb://localhost:27017',
+			dbName: '<%= name %>'
 		},<% } %>
 	}
 }
--- template/conf/test.js
+++ template/conf/test.js
@@ -1,7 +1,8 @@
 module.exports = {
 	mono: {
 		<% if (modules.mongodb) { %>mongodb: {
-			url: 'mongodb://localhost:27017/<%= name %>-test',
+			url: 'mongodb://localhost:27017',
+			dbName: '<%= name %>-test',
 			dropDatabase: true
 		},<% } %>
 	}
